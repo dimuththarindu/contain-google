5e1b326b303198fded9d188268cf1f94990121fe
Dimuth Tharindu <25923940+dimuththarindu@users.noreply.github.com>
2019-06-06 14:11:47 +0530

    Fix the Javascript error

--- background.js
+++ background.js
@@ -281,12 +281,12 @@ function shouldContainInto (url, tab) {
     return false;
   }
   
-if (url.startsWith("https://accounts.youtube.com")) 
+if ((url.startsWith("https://accounts.youtube.com")) 
 	|| (url.startsWith("https://myactivity.google.com")) 
 	|| (url.startsWith("https://history.google.com"))
 	|| (url.startsWith("https://myaccount.google.com"))
 	|| (url.startsWith("https://accounts.google.com")) 
-	|| (url.startsWith("https://mail.google.com")) {
+	|| (url.startsWith("https://mail.google.com"))) {
     // Ignore Gmail
 	// Ignore sign in process
 	// Ignore YouTube signing process
